13b249fa299eb21c55ea7952f9882be324fa0766
friedjoff <friedjoff@geops.de>
2011-12-13 11:17:36 +0100

    update OpenLayers WFS layer to use new layer settings format

--- plugins/layer_types/openlayers_layer_type_geoserver_wfs.js
+++ plugins/layer_types/openlayers_layer_type_geoserver_wfs.js
@@ -25,38 +25,30 @@ Drupal.openlayers.layer.openlayers_layer_type_geoserver_wfs = function(title, ma
     })
   });
   
-  
   // Apply GeoServer SLD.
-  if (typeof options.sld === 'string') {
-    OpenLayers.Request.GET({
-      url: options.sld,
-      success: function(request) {
-        var sld = new OpenLayers.Format.SLD().read(request.responseXML || request.responseText);
-        if (sld.namedLayers[options.protocol.typeName]) {
-          jQuery.each(sld.namedLayers[options.protocol.typeName].userStyles, function(index, style) {
+  var sld = new OpenLayers.Format.SLD().read(options.sld);
+  if (sld.namedLayers[options.protocol.typeName]) {
+    jQuery.each(sld.namedLayers[options.protocol.typeName].userStyles, function(index, style) {
 
-            // Set cursor to pointer.
-            style.defaultsPerSymbolizer = false;
-            style.defaultStyle.cursor = 'pointer';
+      // Set cursor to pointer.
+      style.defaultsPerSymbolizer = false;
+      style.defaultStyle.cursor = 'pointer';
 
-            // Prepend path of external graphics with geoserver_url if path is relative.
-            // This way OpenLayers can find graphics that are stored and used inside GeoServer.
-            jQuery.each(style.rules, function(index, rule) {
-              if (rule.symbolizer.Point &&
-                  rule.symbolizer.Point.externalGraphic &&
-                  rule.symbolizer.Point.externalGraphic.substr(0, 4) != 'http' &&
-                  rule.symbolizer.Point.externalGraphic.substr(0, 1) != '/') {
-                rule.symbolizer.Point.externalGraphic = options.geoserver_url+
-                  'styles/'+rule.symbolizer.Point.externalGraphic;
-              }
-            });
-            
-            // Apply style to layer.
-            layer.styleMap.styles[style.description] = style;
-            layer.redraw();
-          });
+      // Prepend path of external graphics with geoserver_url if path is relative.
+      // This way OpenLayers can find graphics that are stored and used inside GeoServer.
+      jQuery.each(style.rules, function(index, rule) {
+        if (rule.symbolizer.Point &&
+            rule.symbolizer.Point.externalGraphic &&
+            rule.symbolizer.Point.externalGraphic.substr(0, 4) != 'http' &&
+            rule.symbolizer.Point.externalGraphic.substr(0, 1) != '/') {
+          rule.symbolizer.Point.externalGraphic = options.geoserver_url+
+            'styles/'+rule.symbolizer.Point.externalGraphic;
         }
-      }
+      });
+
+      // Apply style to layer.
+      layer.styleMap.styles[style.description] = style;
+      layer.redraw();
     });
   }
   
